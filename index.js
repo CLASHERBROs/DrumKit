aa9027eeb30001402de1e38835aa97d6eb5f0f89
CLASHERBROs <parigames@gmail.com>
2020-05-24 13:46:11 +0530

    Added keyboard functionality

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 var btn = document.getElementsByClassName("drum");
-console.log(btn.array);
+
+
+
 function handleClick(name){
     let nameStr;
     switch(name){
     
         case "w":    nameStr = 'sounds/tom-1.mp3';
+        animate();
         break;
         case "a": nameStr = 'sounds/tom-2.mp3';
+        animate();
         break;
         case "s": nameStr = 'sounds/tom-3.mp3';
+        animate();
         break;
         case "d": nameStr = 'sounds/tom-4.mp3';
+        animate();
         break;
         case "j": nameStr = 'sounds/snare.mp3';
-    break;
+        animate();
+        break;
         case "k": nameStr = 'sounds/crash.mp3';
-    break;
+        animate();
+        break;
         case "l": nameStr = 'sounds/kick-bass.mp3';
-    break;
+        animate();
+        break;
     default: console.log("messed up aint it?");
     }
 var audio = new Audio (nameStr);
 audio.play();
-
+function animate(){
+var animatedBtn = document.querySelector("."+name);
+animatedBtn.classList.add("pressed");
+setTimeout(function(){
+    animatedBtn.classList.remove("pressed")
+},100);
 }
+}
+
+
+
+// Keypress event listener
+document.addEventListener("keydown",function(){
+    handleClick(event.key);
+});
+
+// Button event listener
+
 Array.from(btn).forEach(button => {   //ARRAY.FROM(BTN) CONVERTS BUTTON TO A ARRAY FROM COLLECTION
     button.addEventListener("click",
     function(){handleClick (button.innerHTML)
